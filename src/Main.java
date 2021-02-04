a0655eb218e1159355b8ac7271b2751a77b52dca
Egor Barkovsky <woldermind@gmail.com>
2021-02-04 18:42:23 +0300

    dealer commit

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ VCS -> Enable VSC Integration
 public class Main {
     public static void main(String[] args) {
         System.out.println("initial commit for whorehouse");
+        System.out.println("dealer impl");
     }
 }
